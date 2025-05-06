02a44c0b8817a0e7d8dae0e67278b0d7403faa86
KeertiShidramshettar <keertishettar2011@gmail.com>
2025-05-06 15:22:59 +0530

    Save me

--- Student.java
+++ Student.java
@@ -2,21 +2,24 @@ public class Student {
         String name;
         int srn;
         double marks;
+        int age;
 
-        public Student(String name, int srn, double marks){
+        public Student(String name, int srn, double marks, int age){
          this.name = name;
          this.srn = srn;
          this.marks= marks;
+         this.age = age;
 
         }
         public void displayInfo(){
          System.out.println("Student srn :"+srn);
          System.out.println("Employee Name: "+name);
          System.out.println("Marks $"+marks);
+        System.out.println("Age ;"+age);
         
         }
         public static void main(String[] args) {
-         Student stud1 = new Student("XYZ", 101, 89.00);
+         Student stud1 = new Student("XYZ", 101, 89.00,25);
         stud1.displayInfo();
         }
  }
